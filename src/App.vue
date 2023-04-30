8c589eabc11bf5e23b5ecb9bed7844fc44d68e1a
harymunli <harryrahmad428@gmail.com>
2023-04-30 18:57:05 +0700

    main functionality

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="app">
     <h1>To-Do App</h1>
-    <form>
+    <form @submit.prevent="addTodo">
       <label>New To-Do</label>
-      <input>
+      <input v-model="new_todo" name="new_todo">
       <button>Add To-Do</button>
     </form>
     <h2>To-Do List</h2>
     <ul>
-      <li>
-        learn vue
-        <button>Remove</button>
+      <li v-for="(todo, index) in todos" 
+        :key="index">
+      <span :class="{done: todo.done}"
+        @click="todo.done = !todo.done"
+        >{{todo.content}}</span>
+      <button @click="removeTodo(index)">Remove</button>
       </li>
     </ul>
-    <!-- <h4 v-if="todos.length === 0" >Empty list.</h4> -->
-    <h4>Empty list.</h4>
+    <h4 v-if="todos.length === 0" >Empty list.</h4>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      new_todo : '',
+      todos : [
+        {
+          done: false,
+          content: 'Learn HTML'
+        },
+        {
+          done: false,
+          content: 'Learn Javascript'
+        },
+        {
+          done: false,
+          content: 'Learn Vue'
+        }
+      ]        
+    }
+  },
+  methods: {
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    addTodo(){
+      this.todos.push({ done: false, content: this.new_todo});
+    },
+  }
+}
+</script>
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>

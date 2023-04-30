969d0e01a97e94eee6fc20b151a87527720ef99b
harymunli <harryrahmad428@gmail.com>
2023-04-30 13:32:52 +0700

    kerangka html

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <h1>To-Do App</h1>
+    <form>
+      <label>New To-Do</label>
+      <input>
+      <button>Add To-Do</button>
+    </form>
+    <h2>To-Do List</h2>
+    <ul>
+      <li>
+        learn vue
+        <button>Remove</button>
+      </li>
+    </ul>
+    <!-- <h4 v-if="todos.length === 0" >Empty list.</h4> -->
+    <h4>Empty list.</h4>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
